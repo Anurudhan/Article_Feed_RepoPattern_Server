8df01304b6e44a436769de764ab501c527d30eeb
Anurudhan <anurudhanvaliyakath@gmail.com>
2025-08-03 11:06:34 +0530

    coockies modification

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -27,14 +27,14 @@ export class AuthController {
       res.cookie('accessToken', accessToken, {
         httpOnly: true,
         secure: true,
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge: 15 * 60 * 1000,
       });
 
       res.cookie('refreshToken', refreshToken, {
         httpOnly: true,
         secure: true,
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
 
